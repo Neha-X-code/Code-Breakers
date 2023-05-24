2ca6504bbf6277847f88ad9cc4382d440c2e765b
Neha Arun <nehaarun18@gmail.com>
2023-05-25 02:27:06 +0530

    Updated Timer

--- popup.js
+++ popup.js
@@ -1,35 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get references to the DOM elements
     var eyeReminderButton = document.getElementById('eyeReminderButton');
-    var stretchBreakButton = document.getElementById('stretchBreakButton');
+    var hydrationReminderButton = document.getElementById('hydrationReminderButton');
+    var stretchReminderButton = document.getElementById('stretchReminderButton');
     var moodSelect = document.getElementById('moodSelect');
     var trackMoodButton = document.getElementById('trackMoodButton');
-    var startTimerButton = document.getElementById('startTimerButton');
-    var timerDisplay = document.getElementById('timerDisplay');
-    var minutesInput = document.getElementById('minutesInput');
-    var timerInterval;
   
-    // Add event listeners to the buttons
     eyeReminderButton.addEventListener('click', function() {
-      // Code for eye reminder functionality
-      alert('It\'s time to rest your eyes!');
+      var interval = document.getElementById('eyeInterval').value;
+      startReminderTimer('eye', interval, 'eyeTimerDisplay');
     });
   
-    stretchBreakButton.addEventListener('click', function() {
-      // Code for stretch break functionality
-      alert('Take a moment to stretch and relax your muscles.');
+    hydrationReminderButton.addEventListener('click', function() {
+      var interval = document.getElementById('hydrationInterval').value;
+      startReminderTimer('hydration', interval, 'hydrationTimerDisplay');
+    });
+  
+    stretchReminderButton.addEventListener('click', function() {
+      var interval = document.getElementById('stretchInterval').value;
+      startReminderTimer('stretch', interval, 'stretchTimerDisplay');
     });
   
     trackMoodButton.addEventListener('click', function() {
-      // Code for mood tracking functionality
       var selectedMood = moodSelect.value;
       alert('You selected ' + selectedMood + ' as your current mood.');
     });
   
-    startTimerButton.addEventListener('click', function() {
-      var minutes = parseInt(minutesInput.value);
-      if (!isNaN(minutes) && minutes > 0) {
-        var seconds = minutes * 60;
+    function startReminderTimer(reminderType, interval, timerDisplayId) {
+      var timerDisplay = document.getElementById(timerDisplayId);
+      var timerInterval = timerDisplay.timerInterval;
+      if (timerInterval) {
+        clearInterval(timerInterval);
+      }
+  
+      interval = parseInt(interval, 10);
+      if (!isNaN(interval) && interval > 0) {
+        var seconds = interval * 60;
         timerDisplay.textContent = formatTime(seconds);
   
         timerInterval = setInterval(function() {
@@ -37,16 +42,22 @@ document.addEventListener('DOMContentLoaded', function() {
           timerDisplay.textContent = formatTime(seconds);
           if (seconds <= 0) {
             clearInterval(timerInterval);
-            alert('Timer finished!');
+            showPopup('Timer finished!');
           }
         }, 1000);
+  
+        timerDisplay.timerInterval = timerInterval;
       }
-    });
+    }
   
     function formatTime(seconds) {
       var mins = Math.floor(seconds / 60);
       var secs = seconds % 60;
       return ('0' + mins).slice(-2) + 'm ' + ('0' + secs).slice(-2) + 's';
+    }
+  
+    function showPopup(message) {
+      alert(message);
     }
   });
   
