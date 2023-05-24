867ab4781d890a5b07f5b38838cfa96ec3356db5
Neha Arun <nehaarun18@gmail.com>
2023-05-24 23:32:48 +0530

    Added timer

--- popup.js
+++ popup.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var stretchBreakButton = document.getElementById('stretchBreakButton');
     var moodSelect = document.getElementById('moodSelect');
     var trackMoodButton = document.getElementById('trackMoodButton');
+    var startTimerButton = document.getElementById('startTimerButton');
+    var timerDisplay = document.getElementById('timerDisplay');
+    var minutesInput = document.getElementById('minutesInput');
+    var timerInterval;
   
     // Add event listeners to the buttons
     eyeReminderButton.addEventListener('click', function() {
@@ -21,5 +25,28 @@ document.addEventListener('DOMContentLoaded', function() {
       var selectedMood = moodSelect.value;
       alert('You selected ' + selectedMood + ' as your current mood.');
     });
+  
+    startTimerButton.addEventListener('click', function() {
+      var minutes = parseInt(minutesInput.value);
+      if (!isNaN(minutes) && minutes > 0) {
+        var seconds = minutes * 60;
+        timerDisplay.textContent = formatTime(seconds);
+  
+        timerInterval = setInterval(function() {
+          seconds--;
+          timerDisplay.textContent = formatTime(seconds);
+          if (seconds <= 0) {
+            clearInterval(timerInterval);
+            alert('Timer finished!');
+          }
+        }, 1000);
+      }
+    });
+  
+    function formatTime(seconds) {
+      var mins = Math.floor(seconds / 60);
+      var secs = seconds % 60;
+      return ('0' + mins).slice(-2) + 'm ' + ('0' + secs).slice(-2) + 's';
+    }
   });
   
