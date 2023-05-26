90d88a8b89b3d69c36c20c523cd33ea9d8592bb4
Neha Arun <nehaarun18@gmail.com>
2023-05-26 17:54:28 +0530

    added timer

--- background.js
+++ background.js
@@ -1,32 +1,26 @@
-chrome.alarms.onAlarm.addListener(
-    () => {
-        chrome.notifications.create(
-            // "drink_water",
-            {
-                type: "basic",
-                iconUrl: "icon128.png",
-                title: "Stay Hydrated",
-                message: "Have a sip of water human!"
-            },
-            () => { }
-        )
-    },
-)
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.alarms.onAlarm.addListener(() => {
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: "icon128.png",
+      title: "Stay Hydrated",
+      message: "Have a sip of water, human!"
+    });
+  });
+  
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log(request);
-    if (request.time)
-        createAlarm();
-
-    sendResponse({ success: false }); // Changed to an object instead of a function
-});
-
-
-function createAlarm() {
-    chrome.alarms.create(
-        "drink_water",
-        {
-            delayInMinutes: 1,
-            periodInMinutes: 1
-        }
-    );
-}
+    if (request.time) {
+      createAlarm(request.time);
+      sendResponse({ success: true });
+    } else {
+      sendResponse({ success: false });
+    }
+  });
+  
+  function createAlarm(timerValue) {
+    chrome.alarms.create("drink_water", {
+      delayInMinutes: timerValue,
+      periodInMinutes: timerValue
+    });
+  }
+  
--- popup.js
+++ popup.js
@@ -1,6 +1,31 @@
-const ele = document.getElementById("btn")
-ele.addEventListener("click", () => {
-    chrome.runtime.sendMessage({ time: "1" }, function (response) {
-        console.log(response);
+document.addEventListener("DOMContentLoaded", function() {
+    var btn = document.getElementById("btn");
+    btn.addEventListener("click", function() {
+      var hoursInput = document.getElementById("hours");
+      var minutesInput = document.getElementById("minutes");
+      var hoursValue = parseInt(hoursInput.value);
+      var minutesValue = parseInt(minutesInput.value);
+  
+      // Validation for minutes input
+      if (isNaN(minutesValue) || minutesValue < 0 || minutesValue >= 60) {
+        alert("Please enter a valid value for minutes (0-59).");
+        return;
+      }
+  
+      // Calculate the total time in minutes
+      var totalMinutes = hoursValue * 60 + minutesValue;
+      if (totalMinutes <= 0) {
+        alert("Please enter a valid timer value (greater than 0).");
+        return;
+      }
+  
+      chrome.runtime.sendMessage({ time: totalMinutes }, function(response) {
+        if (response.success) {
+          alert("Alarm set successfully!");
+        } else {
+          alert("Failed to set the alarm.");
+        }
+      });
     });
-});
+  });
+  
