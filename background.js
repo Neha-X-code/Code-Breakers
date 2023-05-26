9442742cf441872b07b02f2a64e6a762567e65fa
Neha Arun <nehaarun18@gmail.com>
2023-05-26 20:25:43 +0530

    eye,water,stretch reminder

--- background.js
+++ background.js
@@ -1,12 +1,36 @@
 chrome.alarms.onAlarm.addListener(function(alarm) {
-  showNotification(alarm.name);
-});
+  var reminderName = alarm.name.split("_")[0];
+  var message = "";
 
-function showNotification(title) {
+  if (reminderName === "Water") {
+    message = "Have a sip of water, human!";
+  } else if (reminderName === "Eye") {
+    message = "Rest your eyes for a while!";
+  } else if (reminderName === "Stretch") {
+    message = "Time to stretch your body!";
+  }
+
   chrome.notifications.create({
     type: "basic",
     iconUrl: "icon128.png",
-    title: title,
-    message: "Time to take a break!"
+    title: reminderName + " Reminder",
+    message: message
+  });
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log(request);
+  if (request.reminder && request.time) {
+    createAlarm(request.reminder, request.time);
+    sendResponse({ success: true });
+  } else {
+    sendResponse({ success: false });
+  }
+});
+
+function createAlarm(reminderName, timerValue) {
+  chrome.alarms.create(reminderName.toLowerCase() + "_reminder", {
+    delayInMinutes: timerValue / 60,
+    periodInMinutes: timerValue / 60
   });
 }
--- popup.js
+++ popup.js
@@ -3,35 +3,43 @@ document.addEventListener("DOMContentLoaded", function() {
   waterBtn.addEventListener("click", function() {
     var waterHoursInput = document.getElementById("waterHours");
     var waterMinutesInput = document.getElementById("waterMinutes");
-    var waterHoursValue = parseInt(waterHoursInput.value) || 0;
-    var waterMinutesValue = parseInt(waterMinutesInput.value) || 1;
-    setReminder("Water Reminder", "Have a sip of water, human!", waterHoursValue, waterMinutesValue);
+    var waterHours = parseInt(waterHoursInput.value);
+    var waterMinutes = parseInt(waterMinutesInput.value);
+    setReminder("Water", waterHours, waterMinutes);
   });
 
   var eyeBtn = document.getElementById("eyeBtn");
   eyeBtn.addEventListener("click", function() {
     var eyeHoursInput = document.getElementById("eyeHours");
     var eyeMinutesInput = document.getElementById("eyeMinutes");
-    var eyeHoursValue = parseInt(eyeHoursInput.value) || 0;
-    var eyeMinutesValue = parseInt(eyeMinutesInput.value) || 1;
-    setReminder("Eye Rest Reminder", "Take a break and rest your eyes.", eyeHoursValue, eyeMinutesValue);
+    var eyeHours = parseInt(eyeHoursInput.value);
+    var eyeMinutes = parseInt(eyeMinutesInput.value);
+    setReminder("Eye Rest", eyeHours, eyeMinutes);
   });
 
-  function setReminder(title, message, hours, minutes) {
-    if (isNaN(hours) || hours < 0 || isNaN(minutes) || minutes <= 0) {
-      alert("Please enter valid values for hours and minutes.");
+  var stretchBtn = document.getElementById("stretchBtn");
+  stretchBtn.addEventListener("click", function() {
+    var stretchHoursInput = document.getElementById("stretchHours");
+    var stretchMinutesInput = document.getElementById("stretchMinutes");
+    var stretchHours = parseInt(stretchHoursInput.value);
+    var stretchMinutes = parseInt(stretchMinutesInput.value);
+    setReminder("Stretch", stretchHours, stretchMinutes);
+  });
+
+  function setReminder(reminderName, hours, minutes) {
+    if ((hours === 0 && minutes === 0) || isNaN(hours) || isNaN(minutes)) {
+      alert("Please enter a valid value for " + reminderName + " reminder.");
       return;
     }
 
     var totalMinutes = hours * 60 + minutes;
-    var delayInMinutes = totalMinutes;
-    var periodInMinutes = totalMinutes;
-
-    chrome.alarms.create(title, {
-      delayInMinutes: delayInMinutes,
-      periodInMinutes: periodInMinutes
+    var totalSeconds = totalMinutes * 60;
+    chrome.runtime.sendMessage({ reminder: reminderName, time: totalSeconds }, function(response) {
+      if (response.success) {
+        alert(reminderName + " reminder set successfully!");
+      } else {
+        alert("Failed to set the " + reminderName + " reminder.");
+      }
     });
-
-    alert("Reminder set successfully!");
   }
 });
