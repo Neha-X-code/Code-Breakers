3c5c3c9187e95212cffae424540e58b56355018e
MariamSG <94617665+MariamSG@users.noreply.github.com>
2023-05-27 16:18:04 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.alarms.onAlarm.addListener(function(alarm) {
-  if (alarm.name=="water_remainder"){
+  if (alarm=="Water"){
     chrome.notifications.create({
       type: "basic",
       iconUrl: "icon1281.png",
       title: "Reminder",
       message: "It's time for " + alarm.name + "!"
   });}
-  else if (alarm.name=="eye rest-remainder"){
+  else if (alarm=="Eye Rest"){
     chrome.notifications.create({
       type: "basic",
       iconUrl: "icon1282.png",
