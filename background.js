9668b0631b739e3e724b5be57fb415a77d8fb005
MariamSG <94617665+MariamSG@users.noreply.github.com>
2023-05-27 16:28:11 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,25 +1,10 @@
 chrome.alarms.onAlarm.addListener(function(alarm) {
-  if (alarm=="Water"){
     chrome.notifications.create({
       type: "basic",
-      iconUrl: "icon1281.png",
+      iconUrl: "cb.jpg",
       title: "Reminder",
       message: "It's time for " + alarm.name + "!"
-  });}
-  else if (alarm=="Eye Rest"){
-    chrome.notifications.create({
-      type: "basic",
-      iconUrl: "icon1282.png",
-      title: "Reminder",
-      message: "It's time for " + alarm.name + "!"
-  });}
-  else {
-    chrome.notifications.create({
-      type: "basic",
-      iconUrl: "icon1283.png",
-      title: "Reminder",
-      message: "It's time for " + alarm.name + "!" 
-  });}
+  });
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
